35b54994219ac7bc7aba019d6fc62ad4f6d4d83f
Baijnath kumar <Baijnath@vebuin.com>
2024-06-29 22:10:24 +0530

    Update some routing cases

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
 
 const Navigation = () => {
-    return (
-      <nav className="container">
-        <div className="logo">
+  return (
+    <nav className="container">
+      <div className="logo">
+        <a href="/">
           <img src="/images/brand_logo.png" alt="logo" />
-        </div>
-        <ul>
-        <li><Link to="/">Home</Link></li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-  
-        <button>login</button>
-      </nav>
-    );
-  };
-  
-  export default Navigation;    
+        </a>
+      </div>
+      <ul>
+        <li>
+          <a href="/home">Menu</a>
+        </li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+
+      <button>login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
